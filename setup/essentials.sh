016d1f1a9287fbdce08bd8e805646255099b5469
Kasper Kronborg <kasper@kronborg.info>
2025-12-23 20:41:37 +0100

    Add setup essentials script

---
+++ setup/essentials.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Install essential tools for archway
+# Usage: ./setup/essentials.sh
+
+set -e
+
+echo "Installing essential tools..."
+sudo pacman -S --noconfirm --needed \
+    base-devel \
+    wget \
+    man-db \
+    stow
+
+echo ""
+echo "Essential tools installed!"
