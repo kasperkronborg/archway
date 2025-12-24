cc70c8681b43fe172df0e165919c71f059856d73
Kasper Kronborg <kasper@kronborg.info>
2025-12-24 13:33:29 +0100

    Update hyprland setup script

--- setup/hyprland.sh
+++ setup/hyprland.sh
@@ -4,10 +4,16 @@
 
 set -e
 
+ARCHWAY_DIR="${ARCHWAY_DIR:-$HOME/archway}"
+
 echo "Installing Hyprland..."
 sudo pacman -S --noconfirm --needed \
     hyprland \
     ghostty
+
+echo "Deploying Hyprland configuration..."
+cd "$ARCHWAY_DIR"
+stow -d dotfiles -t "$HOME" hyprland
 
 echo ""
 echo "Hyprland installation complete!"
